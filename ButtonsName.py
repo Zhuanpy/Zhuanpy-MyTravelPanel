8f76a4215af516f5a7a0699f3e6fa7e7e7b0b1f5
Zhuanpy <zhangzhuan516@gmail.com>
2024-01-16 13:06:06 +0800

    edit convert & koreavisa fun

--- ButtonsName.py
+++ ButtonsName.py
@@ -5,7 +5,7 @@ visa_buttons = {0: ['PDF处理', 'MyVisa.fun01'],
                 3: ['日本签证', 'MyVisa.fun04'],
                 4: ['美国签证', 'MyVisa.fun05'],
                 5: ['中国签证', 'MyVisa.fun06'],
-                6: ['pending', 'MyVisa.fun07'],
-                7: ['pending', 'MyVisa.fun08'],
+                6: ['台湾签证', 'MyVisa.fun07'],
+                7: ['台湾签证', 'MyVisa.fun08'],
                 8: ['pending', 'MyVisa.fun09']}
 
