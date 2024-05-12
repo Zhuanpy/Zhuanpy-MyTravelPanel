9396925ef288f814c8199aebb4dd8dace769ff88
Zhuanpy <zhangzhuan516@gmail.com>
2024-05-12 14:18:00 +0800

    update 05MAY, 2024

--- MyWeb/routes_files.py
+++ MyWeb/routes_files.py
@@ -5,34 +5,22 @@ from Visa.VisaPdfFile.pdffile import MyPdfFile
 fpb = Blueprint('files_routes', __name__)
 
 
-@fpb.route('/pdf_to_pdf', methods=['GET', 'POST'])
+@fpb.route('/pdf_to_pdf', methods=['POST'])
 def merge_pdf_to_pdf():
-    if request.method == 'POST':
-        # 获取输入的文件夹路径
-        folder_path = request.form['folder_path']
-        # 处理图片并生成PDF
-        my_pdf = MyPdfFile(folder_path)
-        my_pdf.merge_images2pdf()
-        my_pdf.merge_pdf2pdf()
-
-        # 返回结果页面
-        return render_template('result.html', folder_path=folder_path)
-
-    # 如果是 GET 请求，返回包含输入框的表单页面
-
-    return render_template('file_processing.html')
+    path = request.form.get('pdfFolderPath')
+    # 处理图片并生成PDF
+    f = MyPdfFile(path)
+    f.merge_pdf2pdf()
+    return render_template('result.html')
 
 
 @fpb.route('/images_to_pdf', methods=['POST'])
 def merge_images_to_pdf():
     # 获取输入的文件夹路径
-    folder_path = request.form.get['imageFolderPath']
-    # file_path = request.form.get("path_create_project")
-    print(folder_path)
+    path = request.form.get('imageFolderPath')
     # 处理图片并生成PDF
-    # my_pdf = MyPdfFile(folder_path)
-    # my_pdf.merge_images2pdf()
-
+    f = MyPdfFile(path)
+    f.merge_images2pdf()
     # 返回结果页面
-    return render_template('result.html', folder_path=folder_path)
+    return render_template('result.html', folder_path=path)
 
